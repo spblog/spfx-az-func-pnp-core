c2c4def9a7cdffc207434abe75ed0dcc3a11811e
Sergei Sergeev <sergeev.srg@gmail.com>
2020-12-09 20:31:50 +0300

    Minor changes.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample code for blog post
+
+Blog post - [How to access SharePoint data from Azure Function with SPFx and PnP Core]()
--- spfx/src/webparts/callPnPCoreApi/components/CallPnPCoreApi.tsx
+++ spfx/src/webparts/callPnPCoreApi/components/CallPnPCoreApi.tsx
@@ -2,24 +2,20 @@ import * as React from 'react';
 import styles from './CallPnPCoreApi.module.scss';
 import { ICallPnPCoreApiProps } from './ICallPnPCoreApiProps';
 import { FC } from 'react';
-import { AadHttpClient, HttpClientResponse } from '@microsoft/sp-http';
+import { AadHttpClient } from '@microsoft/sp-http';
 
 export const CallPnPCoreApi: FC<ICallPnPCoreApiProps> = (props) => {
   const [lists, setLists] = React.useState<any[]>();
 
   React.useEffect(() => {
-    props.context.aadHttpClientFactory
-      .getClient('ca226d3c-f06d-4ea5-8bb4-f7b9b11df7da')
-      .then((client: AadHttpClient): void => {
-        client
-          .get(`https://45c4bdee5fe9.ngrok.io/api/GetLists/?siteUrl=${props.context.pageContext.site.absoluteUrl}&tenantId=${props.context.pageContext.aadInfo.tenantId}`, AadHttpClient.configurations.v1)
-          .then((response: HttpClientResponse): Promise<any[]> => {
-            return response.json();
-          })
-          .then((response: any[]): void => {
-            setLists(response);
-          });
-      });
+    const getData = async () => {
+      const client = await props.context.aadHttpClientFactory.getClient('ca226d3c-f06d-4ea5-8bb4-f7b9b11df7da');
+      const results: any[] = await (await client.get(`https://spfx-pnp-core-api.azurewebsites.net/api/GetLists/?siteUrl=${props.context.pageContext.site.absoluteUrl}&tenantId=${props.context.pageContext.aadInfo.tenantId}`, AadHttpClient.configurations.v1)).json();
+
+      setLists(results);
+    };
+
+    getData();
   }, []);
 
   if (!lists) {
